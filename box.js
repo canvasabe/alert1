80c0ad21799ea6e2cbbddebd678a1deb4461c2d9
canvasabe <szabolcs@occipital.com>
2025-04-25 14:05:38 +0300

    Create box.js

---
+++ box.js
@@ -0,0 +1,9 @@
+const redBox = document.createElement('div');
+redBox.style.position = 'fixed';
+redBox.style.top = '0';
+redBox.style.left = '0';
+redBox.style.width = '100px';
+redBox.style.height = '100px';
+redBox.style.backgroundColor = 'red';
+redBox.style.zIndex = '9999';
+document.body.appendChild(redBox);
